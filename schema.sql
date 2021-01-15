126731a2c950aa48ac23f8b44ffe7736386a177c
Mengmei Tu <redmmeii@gmail.com>
2021-01-15 10:05:54 -0600

    update schema

--- schema.sql
+++ schema.sql
@@ -1,27 +1,28 @@
+DROP DATABASE IF EXISTS employee_management_db;
 CREATE DATABASE employee_management_db;
 
 USE employee_management_db;
 
 CREATE TABLE department (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
+    salary DECIMAL(9,2),
     department_id INTEGER,
     PRIMARY Key (id)
 );
 
 CREATE TABLE employee (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
-    manager_id INT,
+    manager_id INTEGER,
     PRIMARY Key (id)
 );
 
