d39c0f74add4ecf04f698a10303cddddacef0995
Mengmei Tu <redmmeii@gmail.com>
2021-01-17 19:56:03 -0600

    join tables

--- schema.sql
+++ schema.sql
@@ -26,4 +26,29 @@ CREATE TABLE employee (
     PRIMARY Key (id)
 );
 
+SELECT name 
+FROM department 
+LEFT JOIN role 
+ON department.id = role.department_id;
+
+SELECT title, salary, department_id 
+FROM role 
+LEFT JOIN department 
+ON role.department_id = department.id;
+
+SELECT first_name, last_name, role_id, manager_id 
+FROM employee 
+JOIN role 
+ON employee.role_id = role.department_id;
+
+-- SELECT *
+-- FROM department;
+
+-- SELECT *
+-- FROM role;
+
+-- SELECT *
+-- FROM employee;
+
+
 
